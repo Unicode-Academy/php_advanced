1addaa17d55d34a841b1193d0d8b5233621c0702
Hoàng An F8 <hoangan@fullstack.edu.vn>
2023-08-24 01:01:55 +0700

    MVC Case 02

--- mvc_case/configs/routes.php
+++ mvc_case/configs/routes.php
@@ -1,3 +1,5 @@
 <?php
 $routes['default_controller'] = 'homecontroller';
 $routes['users'] = 'usercontroller';
+$routes['auth/login'] = 'authcontroller/login';
+$routes['auth/register'] = 'authcontroller/register';
