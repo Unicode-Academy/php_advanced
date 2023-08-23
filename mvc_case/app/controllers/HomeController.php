fe7bfc86a65039ff619c2d60d754bd2ad0d809ec
Hoàng An F8 <hoangan@fullstack.edu.vn>
2023-08-23 13:30:59 +0700

    MVC Case 01

---
+++ mvc_case/app/controllers/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+//HomeController Controller
+class HomeController extends Controller
+{
+
+    public $data = [], $model = [];
+
+    public function __construct()
+    {
+        //construct
+    }
+
+    public function index()
+    {
+        $this->data['body'] = 'welcome';
+        $this->data['dataView']['pageTitle'] = 'Trang chủ';
+        $this->render('layouts/layout', $this->data);
+    }
+}
---
+++ mvc_case/app/views/users/index.php
@@ -0,0 +1,6 @@
+<h1>{{$pageTitle}}</h1>
+<p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda ad fugit ratione. Quos, placeat? Fugiat quod
+    veritatis numquam ipsum, ducimus atque ratione praesentium reiciendis voluptate consequatur quaerat, quia facere
+    distinctio.
+</p>
