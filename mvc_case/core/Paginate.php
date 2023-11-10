a3d184d23c04a0110ce799e559ee5b988a67e184
Hoàng An F8 <hoangan@fullstack.edu.vn>
2023-11-10 14:21:15 +0700

    Phan trang trong MVC - PHAN 5

--- mvc_case/core/Paginate.php
+++ mvc_case/core/Paginate.php
@@ -1,26 +1,26 @@
 <?php
 
 class Paginate
- {
-    private static function getPaginateLink( $page, $isQuery )
- {
-        if ( !empty( $_SERVER[ 'QUERY_STRING' ] ) && $isQuery ) {
-            $queryString = trim( $_SERVER[ 'QUERY_STRING' ] );
-            parse_str( $queryString, $params );
-            $params[ 'page' ] = $page;
+{
+    private static function getPaginateLink($page, $isQuery)
+    {
+        if (!empty($_SERVER['QUERY_STRING']) && $isQuery) {
+            $queryString = trim($_SERVER['QUERY_STRING']);
+            parse_str($queryString, $params);
+            $params['page'] = $page;
         } else {
-            $params = [ 'page' => $page ];
+            $params = ['page' => $page];
         }
 
-        $link = http_build_query( $params );
-        $link = strpos( $link, '?' ) !== false ? $link : '?'.$link;
+        $link = http_build_query($params);
+        $link = strpos($link, '?') !== false ? $link : '?' . $link;
 
         return $link;
     }
 
-    private static function getView( $data = [] )
- {
-        extract( $data );
+    private static function getView($data = [])
+    {
+        extract($data);
         ob_start();
         require 'core/views/paginate.php';
         $view = ob_get_contents();
@@ -29,28 +29,43 @@ class Paginate
         return $view;
     }
 
-    public static function render( $query, $limit, $page, $isQuery )
- {
+    public static function render($query, $limit, $page, $isQuery)
+    {
 
         $totalRows = $query->rowCount();
 
-        $totalPage = ceil( $totalRows / $limit );
+        $totalPage = ceil($totalRows / $limit);
 
         $self = __CLASS__;
         $pageHtml = '';
 
+        /*
         $begin = $page - 3;
-        if ( $begin <= 0 ) {
+        if ($begin <= 0) {
             $begin = 1;
         }
 
         $end = $page + 3;
-        if ( $end > $totalPage ) {
+        if ($end > $totalPage) {
             $end = $totalPage;
         }
+        */
+        $begin = max(2, $page - 5);
+        $end = min($page + 5, $totalPage);
 
-        $view = self::getView( compact( 'page', 'totalPage', 'self', 'isQuery', 'begin', 'end' ) );
+        $view = self::getView(compact('page', 'totalPage', 'self', 'isQuery', 'begin', 'end'));
 
         return $view;
     }
 }
+
+/*
+page = 1
+1 2 3 4 5 6 ... 20 
+
+page = 10
+1 ... 5 6 7 8 9 10 11 12 13 14 15 ... 20 
+
+page = 19
+1 ... 14 15 16 17 18 19 20 
+*/
--- mvc_case/core/views/paginate.php
+++ mvc_case/core/views/paginate.php
@@ -1,18 +1,38 @@
 <ul class="pagination pagination-sm">
     <?php
     if ($page > 1) {
-        ?>
-    <li class="page-item"><a class="page-link"
-            href="<?php echo $self::getPaginateLink($page - 1, $isQuery); ?>">Trước</a>
+    ?>
+        <li class="page-item"><a class="page-link " href="<?php echo $self::getPaginateLink($page - 1, $isQuery); ?>">Trước</a>
+        </li>
+    <?php } ?>
+    <li class="page-item"><a class="page-link <?php echo $page == 1 ? 'active' : ''; ?>" href="<?php echo $self::getPaginateLink(1, $isQuery); ?>">1</a>
     </li>
-    <?php }?>
-    <?php for ($i = $begin; $i <= $end; $i++) { ?>
-    <li class="page-item "><a class="page-link <?php echo $page == $i ? 'active' : ''; ?>"
-            href="<?php echo $self::getPaginateLink($i, $isQuery); ?>"><?php echo $i; ?></a>
-    </li>
+    <?php
+    if ($begin > 2) {
+    ?>
+        <li class="page-item">
+            <span class="page-link">...</span>
+        </li>
+    <?php
+    }
+    ?>
+    <?php for ($i = $begin; $i < $end; $i++) { ?>
+        <li class="page-item "><a class="page-link <?php echo $page == $i ? 'active' : ''; ?>" href="<?php echo $self::getPaginateLink($i, $isQuery); ?>"><?php echo $i; ?></a>
+        </li>
     <?php  } ?>
-    <?php if ($page < $totalPage) { ?>
-    <li class="page-item"><a class="page-link" href="<?php echo $self::getPaginateLink($page + 1, $isQuery); ?>">Sau</a>
+    <?php
+    if ($end != $totalPage) {
+    ?>
+        <li class="page-item">
+            <span class="page-link">...</span>
+        </li>
+    <?php
+    }
+    ?>
+    <li class="page-item"><a class="page-link <?php echo $page == $totalPage ? 'active' : ''; ?>" href="<?php echo $self::getPaginateLink($totalPage, $isQuery); ?>"><?php echo $totalPage; ?></a>
     </li>
-    <?php }?>
+    <?php if ($page < $totalPage) { ?>
+        <li class="page-item"><a class="page-link" href="<?php echo $self::getPaginateLink($page + 1, $isQuery); ?>">Sau</a>
+        </li>
+    <?php } ?>
 </ul>
