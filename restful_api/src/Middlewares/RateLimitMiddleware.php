b8fb516b96d70f6d338fe30d7785f45270498351
Hoàng An F8 <hoangan@fullstack.edu.vn>
2024-03-29 17:07:29 +0700

    Rate Limit 03

--- restful_api/src/Middlewares/RateLimitMiddleware.php
+++ restful_api/src/Middlewares/RateLimitMiddleware.php
@@ -1,10 +1,12 @@
 <?php
 namespace App\Middlewares;
 
-use App\Models\RateLimit;
+// use App\Models\RateLimit;
 use App\Models\RequestLog;
+use PalePurple\RateLimit\RateLimit;
 use Pecee\Http\Middleware\IMiddleware;
 use Pecee\Http\Request;
+use \PalePurple\RateLimit\Adapter\Stash as StashAdapter;
 
 class RateLimitMiddleware implements IMiddleware
 {
@@ -18,21 +20,31 @@ class RateLimitMiddleware implements IMiddleware
     public function handle(Request $request): void
     {
 
-        $rateLimitModel = new RateLimit();
-        $ip = request()->getIp();
-        $rateLimit = $rateLimitModel->find($ip);
+        // $rateLimitModel = new RateLimit();
+        // $ip = request()->getIp();
+        // $rateLimit = $rateLimitModel->find($ip);
 
-        if ($rateLimit && $rateLimit->request_number >= $this->requestNumber) {
-            $seconds = time() - strtotime($rateLimit->start_time);
-            if ($seconds >= $this->requestPer) {
-                $this->resetRateLimit();
-            } else {
-                errorResponse(status: 429, message: "Rate Limit");
-            }
-        } else {
-            $this->addRequest();
-            $this->updateRateLimit();
-        }
+        // if ($rateLimit && $rateLimit->request_number >= $this->requestNumber) {
+        //     $seconds = time() - strtotime($rateLimit->start_time);
+        //     if ($seconds >= $this->requestPer) {
+        //         $this->resetRateLimit();
+        //     } else {
+        //         errorResponse(status: 429, message: "Rate Limit");
+        //     }
+        // } else {
+        //     $this->addRequest();
+        //     $this->updateRateLimit();
+        // }
+
+        $stash = new \Stash\Pool(new \Stash\Driver\FileSystem());
+        $adapter = new StashAdapter($stash);
+
+        $rateLimit = new RateLimit("limit_ip",  $this->requestNumber, $this->requestPer, $adapter); // 100 Requests / Hour
+
+        $id = $_SERVER['REMOTE_ADDR']; // Use client IP as identity
+        if (!$rateLimit->check($id)) {
+            errorResponse(status: 429, message: "Rate Limit");
+        } 
     }
 
     private function addRequest()
