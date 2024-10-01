5fe1eb8878855e3f1a422ceb99501fdf691b2885
Hoàng An F8 <hoangan@fullstack.edu.vn>
2024-10-02 02:12:50 +0700

    PHP Queue - 02

--- queue/worker.php
+++ queue/worker.php
@@ -1,12 +1,19 @@
 <?php
 require_once 'functions.php';
-$job = getJobFromQueue();
-if ($job) {
-    setJobStatus('process', $job['id']);
-    $data = json_decode($job['job_data'], true);
-    if ($data['type'] === 'sendMail') {
-        //Gọi hàm sendMail
-        sendMail($data['data']['email'], 'Demo gửi mail', $data['data']['message']);
+echo "Queue Starting...\n";
+while (true) {
+    $job = getJobFromQueue();
+    if ($job) {
+        echo "Queue " . $job['id'] . "Processing...\n";
+        setJobStatus('process', $job['id']);
+        $data = json_decode($job['job_data'], true);
+        if ($data['type'] === 'sendMail') {
+            //Gọi hàm sendMail
+            sendMail($data['data']['email'], 'Demo gửi mail', $data['data']['message']);
+        }
+        setJobStatus('done', $job['id']);
+        echo "Queue " . $job['id'] . "Done\n";
     }
-    setJobStatus('done', $job['id']);
+
+    sleep(1);
 }
