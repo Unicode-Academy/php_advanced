c9be139391027cb03a9e9f80663e32618c1c142e
Hoàng An F8 <hoangan@fullstack.edu.vn>
2024-10-02 22:56:56 +0700

    PHP Queue - 04

--- queue/redis.php
+++ queue/redis.php
@@ -4,6 +4,19 @@ use Predis\Client;
 
 require_once 'vendor/autoload.php';
 $client = new Client();
-// $client->set('name', 'hoangan');
-$value = $client->get('name');
-echo $value;
+$job1 = [
+    'type' => 'sendMail',
+    'data' => 'mail1@gmail.com'
+];
+$job2 = [
+    'type' => 'compressImage',
+    'data' => 'anh1.jpg'
+];
+// $client->rPush('task_queue', json_encode($job1));
+// $client->rPush('task_queue', json_encode($job2));
+
+// $task = $client->lPop('task_queue');
+// $task = json_decode($task, true);
+// echo '<pre>';
+// print_r($task);
+// echo '</pre>';
